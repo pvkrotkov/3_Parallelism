0f51cd5306a3ff32019ab046a4a374a90ce71eb5
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-10-15 16:03:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ with open("matrix2.txt", "r") as file:  # чтение второй матриц
 matrix3 = []
 if __name__ == "__main__":
     with Pool(processes=4) as p: #пул с 4 процессами
-        for i in range(len(matrix1)): #каждачя строка матрицы будет рассчитываться синхронно
+        for i in range(len(matrix1)): #каждая строка матрицы будет рассчитываться синхронно
             line = [p.apply_async(element, ((i,j), matrix1, matrix2)) for j in range(len(matrix2))]
             matrix3.append(line)
-        for i in range(len(matrix1)): # распаковываеи результаты вычислений
+        for i in range(len(matrix1)): # распаковываем результаты вычислений
             for j in range(len(matrix2)):
                 matrix3[i][j]=matrix3[i][j].get()
 print(matrix3)
